b336e4674b8f88a1af63315d86be8f455486ed31
Austin <austin.ralls@carvesystems.com>
2020-08-27 15:31:00 -0400

    Use argparse for all argument parsing
    This commit keeps output and behavior the same (or very similar).

--- scanamabob/commands/summary.py
+++ scanamabob/commands/summary.py
@@ -3,10 +3,17 @@ from argparse import ArgumentParser
 from scanamabob.context import Context, add_context_to_argparse
 from scanamabob.services.sts import get_accountid
 
-DESCRIPTION = "Scan AWS environment for common security misconfigurations"
-USAGE = f"""scanamabob summary [-h] [-r regions] [-p profiles]"""
-parser = ArgumentParser(description=DESCRIPTION, usage=USAGE)
-add_context_to_argparse(parser)
+
+DESCRIPTION = 'Scan AWS environment for common security misconfigurations'
+USAGE = f'''scanamabob summary [-h] [-r regions] [-p profiles]'''
+
+
+def add_parser(main_parser):
+    parser = main_parser.add_parser(
+        name='summary', description=DESCRIPTION, usage=USAGE
+    )
+    add_context_to_argparse(parser)
+    parser.set_defaults(func=command)
 
 
 def iam_summary(context):
@@ -17,9 +24,8 @@ def ec2_summary(context):
     print("## EC2")
 
 
-def command(args):
-    """ Main handler of the summary subcommand """
-    arguments = parser.parse_args(args)
+def command(arguments):
+    ''' Main handler of the summary subcommand '''
     context = Context(arguments.profiles, arguments.regions)
 
     if not context.regions_valid():
@@ -34,4 +40,4 @@ def command(args):
         ec2_summary(context)
 
 
-COMMAND = {"description": DESCRIPTION, "function": command}
+COMMAND = {'description': DESCRIPTION, 'add_parser': add_parser, 'function': command}
